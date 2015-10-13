c4c55e4042993ed43e41c8c1e216cd834419e4b1
BennettIronYard <bennettiy12@gmail.com>
2015-10-13 12:04:09 -0400

    Add practice for inheritance

---
+++ src/Animal.java
@@ -0,0 +1,7 @@
+/**
+ * Created by BennettIronYard on 10/13/15.
+ */
+public class Animal {
+    String name;
+
+}
---
+++ src/Condor.java
@@ -0,0 +1,9 @@
+/**
+ * Created by BennettIronYard on 10/13/15.
+ */
+public class Condor extends Animal {
+    @Override
+    public String toString() {
+        return "Condor";
+    }
+}
